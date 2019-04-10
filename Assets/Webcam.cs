a77c9b7fb3e3b4d0100168c21cfe9eb15e64327a
Esurielt <esurielt9917@gmail.com>
2019-04-10 02:17:38 -0700

    init

---
+++ Assets/Webcam.cs
@@ -0,0 +1,14 @@
+﻿// Starts the default camera and assigns the texture to the current renderer
+using UnityEngine;
+using System.Collections;
+
+public class Webcam : MonoBehaviour
+{
+    void Start()
+    {
+        WebCamTexture webcamTexture = new WebCamTexture();
+        Renderer renderer = GetComponent<Renderer>();
+        renderer.material.mainTexture = webcamTexture;
+        webcamTexture.Play();
+    }
+}
