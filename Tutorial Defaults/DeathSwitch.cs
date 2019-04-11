ebfc3006e5687f218505fa2cece2573ba25446c9
Esurielt <esurielt9917@gmail.com>
2019-04-10 21:32:27 -0700

    add candle

---
+++ Tutorial Defaults/DeathSwitch.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using Unity.InteractiveTutorials;
+
+public class DeathSwitch : MonoBehaviour
+{
+    public float delayInSeconds;
+    public Collider playerCollider;
+    public UnityEvent PlayerDie;
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider == playerCollider)
+        {
+            //StopAllCoroutines();
+            PlayerDie.Invoke();
+        }
+    }
+}
